ab2989b0426b803604d85a4fd0968cd863f93897
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-08-07 19:53:58 -0500

    SS -  Add dotenv configuration and environment validation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +12,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     })
   );
-
-  await app.listen(process.env.PORT ?? 3000);
+  let port = envs.port ?? 3000;
+  await app.listen(port);
+  console.log(`App running on port ${port}`);
 }
 bootstrap();
