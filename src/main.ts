3a3a02863ba4efea8d6a863baa4831caf24c3992
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-08-07 20:16:00 -0500

    SS - Prima - SQLite

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger("Main");
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(
@@ -14,6 +15,6 @@ async function bootstrap() {
   );
   let port = envs.port ?? 3000;
   await app.listen(port);
-  console.log(`App running on port ${port}`);
+  logger.log(`App running on port ${port}`);
 }
 bootstrap();
