cd590ec8856dde85f2c4677dec0e70177217fd1e
Santiago Sánchez Esquivel <ssancheze@siesa.com>
2025-08-31 13:49:15 -0500

    SS - Exception handling

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,5 @@
-import { Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { HttpStatus, Injectable, Logger, NotFoundException, OnModuleInit } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from 'generated/prisma';
@@ -47,7 +48,10 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     })
 
     if (!product) {
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException({
+        status: HttpStatus.BAD_REQUEST,
+        message: `Product with id #${id} not found`
+      });
     }
 
     return product
